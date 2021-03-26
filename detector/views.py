90a6a0ce845471893fe655782ed88951afe5f366
magmine <aminemag96@gmail.com>
2021-03-26 17:45:25 +0100

    split responsabilities between multiple classes, and structuring data into dictionaries

--- detector/views.py
+++ detector/views.py
@@ -1,74 +1,88 @@
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from language_detector.settings import BASE_DIR
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-import json
 import os
-import csv
 import codecs
 import dawg
 import re
 
-'''
-read the files one by one a
-'''
 
-# TODO: make the view a class to be able to reuse dawg and other data
-
-
-# TODO: put the name files in a dictionary or something that we can iterate over
-#
-# dansk_file = os.path.join(BASE_DIR,"data/dansk.txt")
-# deutsch_file = os.path.join(BASE_DIR,"data/deutsch.txt")
-# english_file = os.path.join(BASE_DIR,"data/english3.txt")
-# espanol_file = os.path.join(BASE_DIR,"data/espanol.txt")
-# francais_file = os.path.join(BASE_DIR,"data/francais.txt")
-# italiano_file = os.path.join(BASE_DIR,"data/italiano.txt")
-# nederlands_file = os.path.join(BASE_DIR,"data/nederlands3.txt")
-# norsk_file = os.path.join(BASE_DIR,"data/norsk.txt")
-# swiss_file = os.path.join(BASE_DIR,"data/swiss.txt")
+language_files = {
+    'danish': os.path.join(BASE_DIR,"data/dansk.txt"),
+    'german': os.path.join(BASE_DIR,"data/deutsch.txt"),
+    'english': os.path.join(BASE_DIR,"data/english3.txt"),
+    'spanish': os.path.join(BASE_DIR,"data/espanol.txt"),
+    'frensh': os.path.join(BASE_DIR,"data/francais.txt"),
+    'italian': os.path.join(BASE_DIR,"data/italiano.txt"),
+    'dutch': os.path.join(BASE_DIR,"data/nederlands3.txt"),
+    'norsk': os.path.join(BASE_DIR,"data/norsk.txt"),
+    'swiss': os.path.join(BASE_DIR,"data/swiss.txt")
+}
 
 @method_decorator(csrf_exempt, name='dispatch')
 class LanguageResult(APIView):
-    english_file = os.path.join(BASE_DIR,"data/english3.txt")
-    espanol_file = os.path.join(BASE_DIR,"data/espanol.txt")
-
     def __init__(self):
-        self.english_dawg = dawg.DAWG(codecs.open(self.english_file, "r", "ISO-8859-1").read().split("\n")[0:193000])
-        self.espanol_dawg = dawg.DAWG(codecs.open(self.espanol_file, "r", "ISO-8859-1").read().split("\n")[0:170000])
+        self.text : Text = None
         # self.text_english = """This is a test text predominantly written in english but has algo de spanish en el"""
         # self.text_spanish = """Esto es un texto de prueba predominantemente escrito en inglés pero tiene some english in it"""
 
     def post(self, request, format=None):
-        data = {}
         request_data = {'text': request.data.get('text')}
-        data = self.process_text(request_data['text'])
-        data['text'] = request_data['text']
-        return Response(data, status=status.HTTP_200_OK) # Response returns a list
+        input_text = request_data['text']
+        self.text = Text(input_text)
+        stats = self.text.get_language_stats()
+        stats['text'] = input_text
+        return Response(stats, status=status.HTTP_200_OK) # Response returns a list
 
-    def process_text(self, text):
-        text_words = self.get_text_words(text)
+class Text:
+    
+    def __init__(self, text: str):
+        self.text_processor = TextProcessor(text)
+        self.language_stats = {}
+    
+    def get_language_stats(self):
+        return self.text_processor.get_stats()
+
+class TextProcessor:
+
+    def __init__(self, text: str):
+        self.text = text
+        self.languages = Languages()
+
+    def get_stats(self) -> {}:
+        text_words = self.get_text_words()
+        result = self.languages.get_occurances(text_words)
+        result = sorted(result.items(), key=lambda item: item[1], reverse = True)
+        return dict(result)
+
+    def get_text_words(self) -> []:
+        return re.findall(r"\w+", self.text)
+
+class Languages:
+
+    def __init__(self):
+        self.languages_dawg = {}
+        self.init_dawgs(language_files)
+    
+    def get_occurances(self, words: []) -> {}:
         result = {}
-        number_of_english_words = self.count_occurances_dawg(text_words, self.english_dawg)
-        number_of_spanish_words = self.count_occurances_dawg(text_words, self.espanol_dawg)
+        for language, language_dawg in self.languages_dawg.items():
+            result[language] = self.get_occurances_in_dawg(language_dawg, words)
+        return result        
 
-        result['english_words'] = number_of_english_words
-        result['spanish_words'] = number_of_spanish_words
-        if number_of_english_words > number_of_spanish_words:
-            result['language'] = "The predominant language is english"
-        else:
-            result['language'] = "The predominant language is spanish"
-        return result
-
-    def count_occurances_dawg(self, words, language_dawg):
+    def get_occurances_in_dawg(self, language_dawg, words) -> int:
         counter = 0
         for word in words:
             if language_dawg.has_key(word):
-                counter = counter + 1;
-        return counter;
+                counter += 1
+        return counter
 
-    def get_text_words(self, text):
-        return re.findall(r"\w+", text);
+    def init_dawgs(self, language_files: {}):
+        for language, file_path in language_files.items():
+            self.languages_dawg[language] = dawg.DAWG(self.get_language_word_list(file_path))
+    
+    def get_language_word_list(self, file_path):
+        return codecs.open(file_path, "r", "ISO-8859-1").read().split("\n")
