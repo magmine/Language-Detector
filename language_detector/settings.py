cf0be31fb9a972a0ab651eadd48ddf70723a4860
magmine <aminemag96@gmail.com>
2021-03-31 11:16:23 +0100

    preparing settings.py for production

--- language_detector/settings.py
+++ language_detector/settings.py
@@ -21,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bu7#q@0aj+oery+1rt1%sx_i_3$w@asj%)d0w=+m!5071jlbmz'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'bu7#q@0aj+oery+1rt1%sx_i_3$w@asj%)d0w=+m!5071jlbmz')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ["localhost"]
 
